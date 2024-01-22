a9452c245eb230a1ff8b96237297ca7df3dbc437
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-01-22 23:11:22 +0530

    Add files via upload

---
+++ DataTypesDetails.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+
+class DataTypesDetails{
+    public static void main(String args[]){
+
+        int x = 5;
+        System.out.println(Integer.toBinaryString(x));
+        
+        int y = -5;
+        System.out.println(Integer.toBinaryString(y));                         
+    }
+
+}
