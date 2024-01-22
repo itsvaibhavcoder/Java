24df9289c3f282d04aeda094a50093c47bb13969
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-01-22 23:12:27 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+class Variable{
+    public static void main(String args[]){
+        byte b=5;
+        System.out.println(b);
+        //changing value of already declared variable and it work actually
+        b=25;
+        System.out.println(b);  
+        byte c = 130;
+        System.out.println(c); //  it will throw error as range of byte is -128 to 127
+
+    }
+}
