5443ccd45dff075cc1f48953d5d0f217ecec9c0e
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-03-07 23:36:04 +0530

    Create conditional.java

---
+++ 7_Conditional Statement/conditional.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+class conditional{
+    public static void main(String args[]){
+        // if else conditions
+        // if else-if else ladder
+        // switch case
+        // Nested if else 
+    }
+}
