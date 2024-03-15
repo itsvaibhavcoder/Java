dec4a01d0bffca665761df1bb04510e9428e99e6
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-03-15 22:51:27 +0530

    Add files via upload

---
+++ 1 Basic in Java/arithmatic_operators.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+public class arithmatic_operators{
+    public static void main(String[] args){
+        // Coersion
+        byte b = 12;
+        short s = 12;
+        int c = b+s;
+        System.out.println(c);
+    }
+}
